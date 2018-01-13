588662a078aa04fc629bcddadc4ad6ab9ec2a650
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2018-01-13 12:01:25 -0500

    CORS

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
